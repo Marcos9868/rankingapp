4dee3f302231553bbcc1046950f35c224192cb1d
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-02-14 20:53:26 -0300

    Feat: List of movies component

---
+++ ClientApp/src/components/MoviesImage.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import Godfather from '../images/movies/Godfather.png';
+import Highlander from '../images/movies/Highlander.png';
+import HighlanderII from '../images/movies/Highlander2.png';
+import LastOfTheMohicans from '../images/movies/LastOfTheMohicans.png';
+import PoliceAcademy6 from '../images/movies/PoliceAcademy6.png';
+import RearWindow from '../images/movies/RearWindow.png';
+import RoadHouse from '../images/movies/RoadHouse.png';
+import Shawnshank from '../images/movies/Shawshank.png';
+import StarTrek4 from '../images/movies/StarTreck4.png';
+import Superman4 from '../images/movies/Superman4.png';
+
+const MoviesList = [
+  { id: 1, image: Godfather},
+  { id: 2, image: Highlander},
+  { id: 3, image: HighlanderII},
+  { id: 4, image: LastOfTheMohicans},
+  { id: 5, image: PoliceAcademy6},
+  { id: 6, image: RearWindow},
+  { id: 7, image: RoadHouse},
+  { id: 8, image: Shawnshank},
+  { id: 9, image: StarTrek4},
+  { id: 10, image: Superman4},
+]
+
+export default MoviesList;
